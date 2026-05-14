22a99871f0c49e8f4a089b63e984572e2182627d
Luiz Eduardo Costa Fioreti <eduardofioretidev@gmail.com>
2026-05-14 20:57:27 -0300

    Fix: Corrigindo sintaxe do import

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directoryfrom flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
